953426205a08770eac19234767c5e138d8d2395e
jailan-sh <jilan.elsherbiny@gmail.com>
2023-05-13 14:39:31 -0700

    file authors and add libraries to main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_h_
+#define MAIN_h_
+
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
+
+#endif
