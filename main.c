4b543c50f217c58e7a9ae7967aab4d70d38ebfda
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-13 15:27:13 -0700

    start point

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main-function building a simple shell
+ * Return :0(sucess) or error msg(error)
+ * @ac:arguments count
+ * @argv:arguments variables
+ */
+int main(int ac, char **argv)
+{
+	char *shell_prompt = "our_shell$";
+	char *lineptr;
+	size_t n = 0;
+	ssize_t char_num;
+
+	(void) ac ; (void)argv;
+	while (5)
+	{
+		printf("%s", shell_prompt);
+		char_num = getline(&lineptr, &n, stdin);
+		if (char_num == -1)
+		{
+			printf("Exit\n");
+			return (-1);
+		}
+		printf("%s\n", lineptr);
+		free(lineptr);
+	}
+	return(0);
+}
