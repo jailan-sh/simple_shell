6462709ea9e5b78b5a8bd04f8228e633b5f65b24
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-20 12:36:18 -0700

    string_function_2

---
+++ 2-string_func.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: source to copy
+ *
+ *
+ * Return: address where is stored the result
+ * On error: -1 inapropiate entry
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+
+	if (!str)
+		return (NULL);
+
+	arr = malloc((_strlen(str) * sizeof(*arr)) + 1);
+
+	if (!arr)
+		return (NULL);
+
+	_strcpy(arr, str);
+
+	return (arr);
+}
+
+/**
+ * _puts - prints string to std output
+ * @s: string (must be NULL terminated)
+ *
+ * Return: No Return
+ */
+void _puts(char *s)
+{
+	write(1, s, _strlen(s));
+}
