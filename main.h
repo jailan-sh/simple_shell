2df2b452668f0292e462b0e1ba074fb2b61e9279
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-20 12:47:33 -0700

    main.h

--- main.h
+++ main.h
@@ -12,4 +12,23 @@
 #include <errno.h>
 #include <string.h>
 
+extern char **environ;
+
+void execute_command(char** argument, char** env, char* av[]);
+void start_shell(char **av, char **env);
+char *which_like(char *command);
+void execute(char**argv);
+char *_getenv(const char *var);
+void print_env(void);
+
+/**string functions */
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+char *_strdup(char *str);
+void _puts(char *s);
+
 #endif
