e8f32411e577dc46c1cdc3c6e8485224ccfda488
jailn-sh <jailn.elsherbiny@gmail.com>
2023-05-25 06:26:43 -0700

    non

--- non_interactive.c
+++ non_interactive.c
@@ -9,31 +9,40 @@
  *
  * Return: void
  */
+
 void non_interactive(char **av, char **env)
 {
-	char buffer[MAX_ARGU];
-	char *delim = " \t\n\r";
-	int i = 0;
-	char *argument[MAX_ARGU];
+        char buffer[MAX_ARGU];
+        char *delim = " \t\n\r";
+        int i = 0;
+        char *argument[MAX_ARGU];
+	FILE *fp;
 
-	while (fgets(buffer, sizeof(buffer), stdin) != NULL)
-	{
-		i = 0;
-		argument[i] = _strtok(buffer, delim);
-		if (argument[i] == NULL)
-		{
-			continue;
-		}
-		while (argument[i] && i < MAX_ARGU - 1)
-		{
-			if (i > 0 && argument[i] != NULL &&
-					_strcmp(argument[i], "#") == 0)
-			{
-				argument[i] = NULL;
-				break;
-			}
-			argument[++i] = _strtok(NULL, delim);
-		}
-		execute_builtin_command(argument, env, av);
-	}
+	fp = fopen(av[1], "r");
+    	if (!fp)
+    	{
+        perror(av[1]);
+        return;
+    	}
+        while (fgets(buffer, sizeof(buffer), fp) != NULL)
+        {
+                i = 0;
+                argument[i] = _strtok(buffer, delim);
+                if (argument[i] == NULL)
+                {
+                        continue;
+                }
+                while (argument[i] && i < MAX_ARGU - 1)
+                {
+                        if (i > 0 && argument[i] != NULL &&
+                            _strcmp(argument[i], "#") == 0)
+                        {
+                                argument[i] = NULL;
+                                break;
+                        }
+                        argument[++i] = _strtok(NULL, delim);
+                }
+                execute_builtin_command(argument, env, av);
+        }
+	fclose(fp);
 }
